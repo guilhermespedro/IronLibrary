b7fc6b77fa440d1cf5a02a82ad5feb784d65a987
Mariana Duarte <guilhermespedro@outlook.com>
2019-10-04 09:39:57 +0100

    delete admin

--- controllers/admin/logOut.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const Admin = require("../../models/admin");
-
-module.exports = (req, res, next) => {
-  req.session.destroy();
-  res.json({ message: "Admin was logged out" });
-};
