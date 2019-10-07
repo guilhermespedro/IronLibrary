fca1b071ed7c5c87ff7710c36f5174aef11c7b85
Prelhaz <54009534+Prelhaz@users.noreply.github.com>
2019-10-07 12:46:09 +0100

    Update LogIn.jsx

--- client/src/views/LogIn.jsx
+++ client/src/views/LogIn.jsx
@@ -3,56 +3,57 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-// import Cards from "react-bootstrap/Cards";
-
-// import * as AuthenticationServices from "./../services/auth-service";
+import { logIn as logInService } from "./../services/authentication-api";
 
 export default class LogInView extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       email: "",
-  //       password: ""
-  //     };
-  //     this.onValueChange = this.onValueChange.bind(this);
-  //     this.onFormSubmit = this.onFormSubmit.bind(this);
-  //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
-  //   onValueChange(event) {
-  //     const name = event.target.name;
-  //     const value = event.target.value;
-  //     this.setState({
-  //       [name]: value
-  //     });
-  //   }
+  onValueChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
 
-  //   onFormSubmit(event) {
-  //     event.preventDefault();
-  //     const { email, password } = this.state;
-  //     AuthenticationServices.logInService({
-  //       email,
-  //       password
-  //     })
-  //       .then(user => {
-  //         this.props.history.push("/profile");
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       });
-  //   }
+  onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    logInService({
+      email,
+      password
+    })
+      .then(user => {
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
       <div>
         <div className="d-flex justify-content-center p-5">
-          <Form className="w-25 p-3 block-example border border-dark ">
+          <Form
+            className="w-25 p-3 block-example border border-dark "
+            onSubmit={this.onFormSubmit}
+          >
             <Form.Group size="sm">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 placeholder="Email"
                 name="email"
-                //   value={this.state.email}
-                //   onChange={this.onValueChange}
+                value={this.state.email}
+                onChange={this.onValueChange}
               />
             </Form.Group>
             <Form.Group size="sm">
@@ -61,8 +62,8 @@ export default class LogInView extends Component {
                 placeholder="Password"
                 name="password"
                 type="password"
-                //   value={this.state.password}
-                //   onChange={this.onValueChange}
+                value={this.state.password}
+                onChange={this.onValueChange}
               />
             </Form.Group>
             <Button type="submit">Log In</Button>
