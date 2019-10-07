c88d7773a3f472be1a24fecd785a6ab4fb7d3c8f
Mariana Duarte <guilhermespedro@outlook.com>
2019-10-07 12:46:38 +0100

    book comp and view

--- client/src/component/bookCard.jsx
+++ client/src/component/bookCard.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 
 export default class bookCard extends Component {
   render() {
     return (
       <div>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src={this.props.book.url} />
           <Card.Body>
-            <Card.Title>Book Title</Card.Title>
-            <Card.Text>Book Description</Card.Text>
+            <Card.Title>{this.props.book.title}</Card.Title>
+            <Card.Text>{this.props.book.description}</Card.Text>
             <Button variant="primary">Read the Book</Button>
           </Card.Body>
         </Card>
--- client/src/views/SingleBook.jsx
+++ client/src/views/SingleBook.jsx
@@ -1,42 +1,33 @@
 import React, { Component } from "react";
-import Card from "react-bootstrap/Card";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
-import ListGroup from "react-bootstrap/ListGroup";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import BookCard from "../component/bookCard";
+import { getOneBook } from "../services/book-api";
 
-export default class SingleBook extends Component {
+export default class singleBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: null
+    };
+  }
+
+  componentDidMount() {
+    getOneBook(this.props.match.params.id)
+      .then(book => {
+        this.setState({
+          book: book
+        });
+      })
+      .catch(error => {
+        console.log(erro);
+      });
+  }
+
   render() {
+    if (!this.state.beer) return <div>Loading...</div>;
     return (
-      <div className="d-flex justify-content-center p-5">
-        <Card style={{ width: "50rem" }}>
-          <Row>
-            <Col>
-              <Card.Img
-                variant="top"
-                src="https://s2982.pcdn.co/wp-content/uploads/2014/08/HP_hc_new_6.jpeg"
-              />
-            </Col>
-            <Col>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Description : Some quick example text to build on the card
-                  title and make up the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                {/* <ListGroupItem>Description</ListGroupItem> */}
-                <ListGroupItem>Category</ListGroupItem>
-                <ListGroupItem>Year</ListGroupItem>
-                <ListGroupItem>ISBN</ListGroupItem>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="#">Buy now 14.50€</Card.Link>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card>
+      <div className="bookCard">
+        <h1> Book Description </h1>
+        <BookCard book={this.state.book} />
       </div>
     );
   }
