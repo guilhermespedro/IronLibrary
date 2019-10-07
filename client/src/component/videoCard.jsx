152f493cd5a53ceb674281ed89dbad2277bc1246
Mariana Duarte <guilhermespedro@outlook.com>
2019-10-07 15:48:43 +0100

    library and videocard

--- client/src/component/videoCard.jsx
+++ client/src/component/videoCard.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export default class videoCard extends Component {
   render() {
     return (
       <div>
-        <Card style={{ width: "18rem" }}>
+        <Card style={{ width: "14rem" }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title>Video Title</Card.Title>
             <Card.Text>Video Description</Card.Text>
-            <Button variant="primary">Watch the Video</Button>
+            <Button variant="outline-primary">Watch the Video</Button>
           </Card.Body>
         </Card>
       </div>
