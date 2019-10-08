05b79914c9115786ce2c9be4ab0160c0ddcb41d6
André Almeida <dealmeida_andre@hotmail.com>
2019-10-08 12:01:01 +0100

    React Player

---
+++ client/src/component/PlayerVideo.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class PlayerVideo extends Component {
+    render() {
+        return (
+            <div>
+                <div className='react-videos'>
+        <ReactPlayer
+          url={this.video.url}
+          title= {this.video.title}
+          description= {this.video.description}
+          duration= {this.video.duration}
+          className='react-player'
+          playing
+          width='100%'
+          height='100%'
+        />
+      </div>
+            </div>
+        )
+    }
+}
