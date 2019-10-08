e7f3382252c84b00e832f1bf96f12d10efaf4c17
Mariana Duarte <guilhermespedro@outlook.com>
2019-10-08 12:34:22 +0100

    reac-player working

--- client/src/component/PlayerVideo.jsx
+++ client/src/component/PlayerVideo.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import ReactPlayer from 'react-player'
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
 
 export default class PlayerVideo extends Component {
-    render() {
-        return (
-            <div>
-                <div className='react-videos'>
-        <ReactPlayer
-          url={this.video.url}
-          title= {this.video.title}
-          description= {this.video.description}
-          duration= {this.video.duration}
-          className='react-player'
-          playing
-          width='100%'
-          height='100%'
-        />
+  render() {
+    return (
+      <div>
+        <div className="react-videos">
+          <ReactPlayer
+            url={this.video.url}
+            title={this.video.title}
+            description={this.video.description}
+            duration={this.video.duration}
+            className="react-player"
+            playing
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
-            </div>
-        )
-    }
+    );
+  }
 }
--- client/src/component/videoCard.jsx
+++ client/src/component/videoCard.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-// import "node_modules/video-react/dist/video-react.css";
-import { Player } from "video-react";
+import ReactPlayer from "react-player";
 
 export default class videoCard extends Component {
   render() {
     return (
       <div>
         <Card style={{ width: "14rem" }}>
-          <Player
-            playsInline
-            poster="/assets/poster.png"
-            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            width="100%"
+            height="10%"
           />
-          {/* <Card.Img
-            variant="top"
-            src="https://www.youtube.com/watch?v=Ke90Tje7VS0"
-          /> */}
           <Card.Body>
             <Card.Title>Video Title</Card.Title>
             <Card.Text>Video Description</Card.Text>
