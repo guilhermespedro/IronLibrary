22b35ad2c237846e877dbeeb61e7bed65838019e
André Almeida <dealmeida_andre@hotmail.com>
2019-10-10 20:03:46 +0100

    Update Home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
 import BookCard from '../component/bookCard';
-import VideoCard from './../component/videoCard';
+import VideoCard from '../component/videoCard';
 
 import { list as listBookApi } from './../services/book-api';
 import { list as listVideoApi } from './../services/video-api';
